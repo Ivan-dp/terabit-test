8f132068f5bc73eb7dc27ba1374aef5ba1594864
Ivan-dp <ivan-dp@bk.ru>
2022-06-28 19:27:32 +0300

    fixed the bug about stop the setCount. Rewrite setTimeout, and onClick={}

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,22 +9,23 @@ const Counter = (props) => {
   const [toggle, setToggle] = useState(true);
   const interval = props.interval;
 
-  let countId = setTimeout(() => {
+  function fn1() {
     if (toggle) {
       setCount(count + 1);
     }
-  }, interval);
+  }
 
-  if (!toggle) {
-    clearTimeout(countId);
+  let idСount = setTimeout(fn1, interval);
+
+  function fn2() {
+    setToggle(!toggle);
+    clearTimeout(idСount);
   }
 
   return (
     <div className="counter">
       <h2>#{count}: текущий_счётчик</h2>
-      <button onClick={() => setToggle(!toggle)}>
-        {toggle ? "Остановить" : "Запустить"}
-      </button>
+      <button onClick={fn2}>{toggle ? "Остановить" : "Запустить"}</button>
     </div>
   );
 };
